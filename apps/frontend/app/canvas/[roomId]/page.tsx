f71bd0485324e4c5c7a0b52108aad0953239b565
kiranrega <kirankumar.rega@gmail.com>
2026-04-25 19:47:01 +0530

    [200~added optional chainging ~

--- apps/frontend/app/canvas/[roomId]/page.tsx
+++ apps/frontend/app/canvas/[roomId]/page.tsx
@@ -1,6 +1,6 @@
 import RoomCanvas from "@/components/RoomCanvas";
 
 export default function CanvasPage({ params }: { params: { roomId: string } }) {
-  const roomId = params.roomId;
+  const roomId = params?.roomId;
   return <RoomCanvas roomId={roomId} />;
 }
